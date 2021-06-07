5c175fac1a8ca7ac696e731bc587d4706c1e0b49
unknown <kirankharvi22@gmail.com>
2021-06-07 20:19:29 +0530

    completed 100%

--- Quizzy/Quizzy/settings.py
+++ Quizzy/Quizzy/settings.py
@@ -44,8 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
-    'PIL',
     'st_quiz',
     'st_account',
 ]
