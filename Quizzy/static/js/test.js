9b16e08fab204549bfc48c1a53940aa442e0ede5
unknown <kirankharvi22@gmail.com>
2021-06-07 18:53:26 +0530

    final commit

--- Quizzy/static/js/test.js
+++ Quizzy/static/js/test.js
@@ -1,16 +1,14 @@
 
 
+
 $(document).ready(function()
 {
-    console.log(question_set)
-
-    console.log(correct_ans)
-    console.log(correct_ans_index)
-    $("#question").text(q_no+") "+question_set[q_no-1].question.replaceAll("&#039;","\'"))
-    $("#0").text(question_set[q_no-1].answers[0])
-    $("#1").text(question_set[q_no-1].answers[1])
-    $("#2").text(question_set[q_no-1].answers[2])
-    $("#3").text(question_set[q_no-1].answers[3])
+    
+    $("#question").text(q_no+1+") "+question_set[q_no].question.replaceAll("&#039;","\'").replaceAll('&amp;','&').replaceAll('&lt;','<').replaceAll('&gt;','>').replaceAll('&quot;','\"'));
+    $("#0").text(question_set[q_no].answers[0].replaceAll("&#039;","\'").replaceAll('&amp;','&').replaceAll('&lt;','<').replaceAll('&gt;','>').replaceAll('&quot;','\"'));
+    $("#1").text(question_set[q_no].answers[1].replaceAll("&#039;","\'").replaceAll('&amp;','&').replaceAll('&lt;','<').replaceAll('&gt;','>').replaceAll('&quot;','\"'));
+    $("#2").text(question_set[q_no].answers[2].replaceAll("&#039;","\'").replaceAll('&amp;','&').replaceAll('&lt;','<').replaceAll('&gt;','>').replaceAll('&quot;','\"'));
+    $("#3").text(question_set[q_no].answers[3].replaceAll("&#039;","\'").replaceAll('&amp;','&').replaceAll('&lt;','<').replaceAll('&gt;','>').replaceAll('&quot;','\"'));
         
 })
 
@@ -20,6 +18,7 @@ $(".test").click(
     {
 
         $(".test").removeClass("active")
+        $(this).removeClass("btn-light")
         $(this).addClass("active")
         $("#next_btn").removeClass("d-none")
         
@@ -30,8 +29,6 @@ $("#next_btn").click(
     function()
     {
         console.log(q_no)
-        if(!(q_no> 10))
-        {
         var selected ='' 
         $('.test').each(function (){
             if ($(this).hasClass("active"))
@@ -57,67 +54,83 @@ $("#next_btn").click(
 
                 
                 }
-
+                if(q_no<(total_question-1))
+                {
                 setTimeout(
                 function(){
-                correct_ans = question_set[q_no-1].correct_answer
-                correct_ans_index = question_set[q_no-1].answers.indexOf(correct_ans)
-                $("#question").text(q_no+") "+question_set[q_no].question.replaceAll("&#039;","\'"))
-                $("#0").text(question_set[q_no-1].answers[0])
-                $("#1").text(question_set[q_no-1].answers[1])
-                $("#2").text(question_set[q_no-1].answers[2])
-                $("#3").text(question_set[q_no-1].answers[3])
                 q_no = q_no+1
+                correct_ans = question_set[q_no].correct_answer
+                console.log(correct_ans)
+                correct_ans_index = question_set[q_no].answers.indexOf(correct_ans)
+                $("#question").text(q_no+1+") "+question_set[q_no].question.replaceAll("&#039;","\'").replaceAll('&amp;','&').replaceAll('&lt;','<').replaceAll('&gt;','>').replaceAll('&quot;','\"'));
+                $("#0").text(question_set[q_no].answers[0].replaceAll("&#039;","\'").replaceAll('&amp;','&').replaceAll('&lt;','<').replaceAll('&gt;','>').replaceAll('&quot;','\"'));
+                $("#1").text(question_set[q_no].answers[1].replaceAll("&#039;","\'").replaceAll('&amp;','&').replaceAll('&lt;','<').replaceAll('&gt;','>').replaceAll('&quot;','\"'));
+                $("#2").text(question_set[q_no].answers[2].replaceAll("&#039;","\'").replaceAll('&amp;','&').replaceAll('&lt;','<').replaceAll('&gt;','>').replaceAll('&quot;','\"'));
+                $("#3").text(question_set[q_no].answers[3].replaceAll("&#039;","\'").replaceAll('&amp;','&').replaceAll('&lt;','<').replaceAll('&gt;','>').replaceAll('&quot;','\"'));
                 $(".test").removeClass("active")
                 $(".test").removeClass("btn-success")
                 $(".test").removeClass("btn-danger")
                 $(".test").addClass("btn-light")
                 $("#next_btn").addClass("d-none")
-
-
-
                  }
                 , 1000);
+                }
+            else
+            {
+                setTimeout(function()
+                {
+                
+
+                $("#ans_block").hide();
+                $("#question").addClass("d-none")
+                var end = new Date().getTime();
+                var display_string =""
+                var result_string = "You have Scored: "+correct_ans_count+" marks out of 50 marks."
+                let time_taken = time(start,end)
+                let time_array = time_taken.split(":")
+                hour = time_array[0]
+                min = time_array[1]
+                sec = time_array[2]
+                $("#score").removeClass("d-none")
+                $("#time_taken").removeClass("d-none")
+                if(parseInt(hour)>0)
+                {
+                    display_string = "You took "+hour+" hours "+min+" minutes "+sec+" second to complete the test"
+                    $("#score").text(result_string)
+                    $("#time_taken").text(display_string)
+
+                }
+                else if(parseInt(min)>0)
+                {
+                    display_string = "You took "+min+" minutes "+sec+" second to complete the test"
+                    $("#score").text(result_string)
+                    $("#time_taken").text(display_string)
+                }
+                else
+                {
+                    display_string = "You took "+sec+" second to complete the test"
+                    $("#score").text(result_string)
+                    $("#time_taken").text(display_string)
+                }
             }
-        })
+                ,1000)
 
-    }
-    else
-    {
-        $("#ans_block").hide();
+                setTimeout(function()
+                {
+                    window.location="http://127.0.0.1:8000/";
 
-        var end = new Date().getTime();
-        var display_string =""
-        var result_string = "You have Scored: "+correct_ans_count+" marks out of 50 marks."
-        let time_taken = time(start,end)
-        let time_array = time_taken.split(":")
-        hour = time_array[0]
-        min = time_array[1]
-        sec = time_array[2]
-        $("#score").removeClass("d-none")
-        if(parseInt(hour)>0)
-        {
-            display_string = "You took "+hour+" hours "+min+" minutes "+sec+" second to complete the test"
-            $("#score").text(result_string)
-            $("#question").text(display_string)
-        }
-        else if(parseInt(min)>0)
-        {
-            display_string = "You took "+min+" minutes "+sec+" second to complete the test"
-            $("#score").text(result_string)
-            $("#question").text(display_string)
-        }
-        else
-        {
-            display_string = "You took "+sec+" second to complete the test"
-            $("#score").text(result_string)
-            $("#question").text(display_string)
-        }
-        
+                },5000)
 
-    }
-}
-)
+             }
+            }
+
+        }
+        )
+    })
+
+    
+    
+
 
 function time(start,end) {
     var duration = end-start
